1d1632ab082e808d90ad11dda5960bf4aa90d3b8
Jon Foust <jonfoust@google.com>
2020-12-14 15:06:52 -0500

    Initial commit

---
+++ Game Client/Program.cs
@@ -0,0 +1,75 @@
+﻿// Copyright 2020 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Gamefrontend;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                Console.WriteLine("The client connected successfully");
+
+            });
+
+            var client = new GameFrontendMMService.GameFrontendMMServiceClient(channel);
+
+            var info = new ClientInfo()
+            {
+
+                Id = GeneratePlayerId()
+
+            };
+
+            // TO DO
+            // Add additonal information for request such as latency, region, etc
+
+
+            var request = new ClientMatchMakingRequest() { Info = info };
+
+            var response = client.FindMatch(request);
+
+            Console.WriteLine($"This client received an assignment to the server located at {response.Assignment.Connection}");
+
+
+            // TO DO
+            // Connect to Game Server with assignment/address received
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+
+        static private Int32 GeneratePlayerId()
+        {
+            Random rnd = new Random();
+            int playerId = rnd.Next(00000001, 10000000);
+
+            return playerId;
+        }
+    }
+}
---
+++ Game Frontend/GameFrontendMMServiceImpl.cs
@@ -0,0 +1,52 @@
+﻿// Copyright 2020 Google Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Gamefrontend;
+using Grpc.Core;
+using OpenMatch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static Gamefrontend.GameFrontendMMService;
+
+namespace Game_Frontend
+{
+    class GameFrontendService : GameFrontendMMServiceBase
+    {
+        public override Task<GameFrontendResponse> FindMatch(ClientRequest request, ServerCallContext context)
+        {
+            // TO DO:
+            Console.WriteLine($"Matchmaking request received from player with id {request.Info.Id.ToString()}");
+            // Access player information and generate ticket using player data
+            // Submit ticket to Open Match and wait for assignment
+
+            // Send assignment response to game client
+            Assignment response = new Assignment() { Connection = GenerateConnAddr() }; // This is used for example purposes and GenerateConnAddr to create a dummy connection string
+
+            Console.WriteLine($"Assigning the client the following address: {response.Connection}");
+
+            return Task.FromResult(new GameFrontendResponse() { Assignment = response});
+        }
+
+        // This function is used to generate a dummy ip address to represent a connection string to a game server
+        private string GenerateConnAddr()
+        {
+            Random r = new Random();
+            return r.Next(0,256) + "." + r.Next(0, 256) + "." + r.Next(0, 256) + "." + r.Next(0, 256);
+        }
+    }
+}
